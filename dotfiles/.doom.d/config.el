8e4ce694c3f7ebf57e5a6e3407246a957e42ac83
Tiger Nie <nhl0819@gmail.com>
2020-04-22 14:00:01 -0500

    Updated config 2020-04-22T14:00:01-05:00

--- dotfiles/.doom.d/config.el
+++ dotfiles/.doom.d/config.el
@@ -57,3 +57,5 @@
 (setq doom-modeline-python-executable "python3")
 (setq python-shell-interpreter "python3"
       flycheck-python-pycompile-executable "python3")
+
+(setq doom-modeline-env-python-executable "python3")
