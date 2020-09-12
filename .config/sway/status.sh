ef7e857d3af076ab300aaa4658b44243693815ba
Tiger Nie <nhl0819@gmail.com>
2020-09-13 03:07:35 +0800

    Updated config 2020-09-13T03:07:35+08:00

---
+++ .config/sway/status.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# The Sway configuration file in ~/.config/sway/config calls this script.
+# You should see changes to the status bar after saving this script.
+# If not, do "killall swaybar" and $mod+Shift+c to reload the configuration.
+
+# The abbreviated weekday (e.g., "Sat"), followed by the ISO-formatted date
+# like 2018-10-06 and the time (e.g., 14:01)
+date_formatted=$(date "+%a %F %H:%M:%S")
+
+# Returns the battery status: "Full", "Discharging", or "Charging".
+battery_status=$(
+    awk '{
+    if ($0 == "Charging") {
+        print "🔌"
+    } else if ($0 == "Discharging") {
+        print "⚡"
+    } else {
+        print "Unknown"
+    }
+    }' /sys/class/power_supply/BAT0/status
+)
+battery_level=$(cat /sys/class/power_supply/BAT0/capacity)%
+
+# Brightness status
+brightness="$(brightnessctl -m | sed 's/,/ /g' | awk '{print $4}') 💡"
+
+# Volume status
+volume=$(amixer -M get Master | \
+    awk '/Front Left: Playback/ \
+        {print $6=="[off]" ? $5" 🔇" : $5" 🔈"}' | \
+    tr -d "[]") 
+
+# Emojis and characters for the status bar
+# ↑💎 💻 💡 🔌 ⚡ 📁 \|
+echo $volume $brightness $battery_level $battery_status $date_formatted
